ef91ff923f044a3b2995e2c7ec1ec99d098c22a2
DiegoDev <diegorego516@gmail.com>
2026-04-02 12:05:10 -0300

    Add files via upload

---
+++ hacker_hub.py
@@ -0,0 +1,47 @@
+import socket
+
+def servidor():
+    HOST = "127.0.0.1"
+    PORT = 5000
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST,PORT))
+    s.listen(1)
+    print("Servidor aguardando conexão...")
+
+    conn, addr = s.accept()
+    print("conectado por", addr)
+
+    while True:
+        data = conn.recv(1024).decode("utf-8")
+        if not data or data.lower() == "sair":
+            break
+        print("Mensagem recebida")
+        conn.send(f"Echo: {data}".encode("utf-8"))
+
+    conn.close()
+    s.close()
+
+def cliente():
+    HOST = "127.0.0.1"
+    PORT = 5000
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST,PORT))
+
+    while True:
+        msg = input("Digite uma mensagem ('sair' para encerrar): ")
+        s.send(msg.encode("utf-8"))
+        if msg.lower() == "sair":
+            break
+        resposta = s.recv(1024).decode("utf-8")
+        print("Servidor respondeu: ", resposta)
+
+    s.close
+
+if __name__ == "__main__":
+    escolha = input("Digite 's' para servidor ou 'c' para cliente: ")
+    if escolha == "s":
+        servidor()
+    else:
+        cliente()
