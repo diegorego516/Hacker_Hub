4d471e658925cd2180e5a2b918fe43bbec17499a
DiegoDev <diegorego516@gmail.com>
2026-04-02 16:54:06 -0300

    Update hacker_hub.py

--- hacker_hub.py
+++ hacker_hub.py
@@ -45,3 +45,37 @@ if __name__ == "__main__":
         servidor()
     else:
         cliente()
+
+import socketserver
+
+def tcp_ip():
+    HOST = "127.0.0.1"
+    PORT = 5000
+
+    s = socket.socket(socketserver.TCPServer, socket.SOCK_STREAM)
+    s.bind((HOST,PORT))
+    s.listen(1)
+    print("Servidor conectado com sucesso...")
+
+    conn, addr = s.accept()
+    print("conectado por", addr)
+
+    while True:
+        data = conn.recv(1024).decode("utf-8")
+        if not data or data.lower() == "sair":
+            break
+        print("Conexão aceita, conectando o usuário...")
+        def connect():
+            conn.connect(f"Echo: {connect}".encode("utf-8"))
+    
+        conn.close()
+        s.close()
+
+if __name__ == "__main__":
+    escolha = input("Digite 't' para tcp-ip, 's' para servidor e 'c' para cliente")
+    if escolha == "s":
+        tcp_ip()
+    try:
+        servidor()
+    except Exception as e:
+        cliente()
